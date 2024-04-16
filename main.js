5315e14107c3d0c97403e399382f19100712fae2
Felpsssss07 <114917900+Felpsssss07@users.noreply.github.com>
2024-04-16 08:16:50 -0300

    k

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ for(let i = 0; i<botoes.length; i++) {
     }
 }
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo = new Date(2025-01-01);
+const tempoObjetivo = new Date("2025-01-28T00:00:00");
 1et tempoAtual = new Date();
 
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
